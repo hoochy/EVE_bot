32fdc85489644efcfe80207fd801f89d79b097ae
hoochy <painmaster@mail.ru>
2014-04-25 22:32:37 +0800

    играюсь с гит

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ def ReadConfig(options):
     return True
 
 def ttt():
-    print('10 seconds left')
+    print('120 seconds left')
 
 options = {}
 
@@ -110,7 +110,7 @@ if ReadConfig(options):
         # if xmpp.connect(('talk.google.com', 5222)):
         #     ...
         xmpp.process(block = False)
-        xmpp.schedule('testschedule', 10, ttt,repeat = True)
+        xmpp.schedule('testschedule', 120, ttt,repeat = True)
         print("Done")
     else:
         print("Unable to connect.")
